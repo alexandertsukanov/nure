bfb74990efc2f6af1fae1601e6f32d16577570fa
Alexander <alexander.tsukanovvv@gmail.com>
2016-10-18 18:10:22 +0300

    First Commit

---
+++ ConsoleApplication1/task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Variant # 2
+// Task # 1
+
+namespace task1
+{
+    class task1
+    {
+        static void Main(string[] args)
+        {
+            Random a = new Random();
+            int answer = 0;
+            for (int i = 0; i < 15; i++) {
+                int b = a.Next(1, 100);
+                if ((b % 7 == 0) && (b % 3 == 0)) {
+                    answer += b;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
